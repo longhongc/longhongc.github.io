5bfb2742b4ba31f5787790a1d10aef08fd3e7418
Chang-Hong Chen <longhongc@gmail.com>
2023-02-24 13:46:30 -0500

    Add self intro

--- source/conf.py
+++ source/conf.py
@@ -20,13 +20,28 @@ templates_path = ['_templates']
 exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
-
 # -- Options for HTML output -------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
+
+html_theme_options = {
+    'prev_next_buttons_location': 'None',
+    # Toc options
+    'collapse_navigation': False,
+    'sticky_navigation': True,
+    'navigation_depth': 4,
+}
+
+# html_theme = 'otc_tcs_sphinx_theme'
+# html_theme_path = ['_themes']
 html_static_path = ['_static']
+
+html_css_files = ['custom.css']
 
 
 html_logo = 'images/eye_human.png'
 html_favicon = 'images/eye.png'
+
+
+html_show_sourcelink = False
