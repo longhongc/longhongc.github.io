9746e6dd76aa5cc8f94ee0673db59ac20a19c7bf
Chang-Hong Chen <longhongc@gmail.com>
2023-02-26 20:59:17 -0500

    Update contact form

--- source/conf.py
+++ source/conf.py
@@ -9,7 +9,7 @@
 project = 'longhongc.github.io'
 copyright = '2023, Chang-Hong Chen'
 author = 'Chang-Hong Chen'
-release = '0.0.1'
+release = '0.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
